291ba48b8a6b0a79429437eda2205ab85a083155
Tamas Nepusz <ntamas@gmail.com>
2022-03-28 00:54:15 +0200

    chore: bumped version to 1.14.2

--- src/flockwave/ext/version.py
+++ src/flockwave/ext/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave extension manager package."""
 
-__version_info__ = (1, 14, 1)
+__version_info__ = (1, 14, 2)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
